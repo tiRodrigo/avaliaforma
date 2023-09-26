5bb597fafa24a5e3a02667c091ad303e3f2af2a8
Aluno <aluno@edu.senac.br>
2023-09-26 20:34:24 -0300

    first commit

---
+++ js/texto_abertura.js
@@ -0,0 +1,21 @@
+function inicio() {
+    const textoOriginal = "Planeta Alien";
+    const elementoTexto = document.querySelector("p")
+    const audio = document.querySelector("audio")
+    const btn = document.querySelector("button")
+    const atrasoRapido = 5000 // Atraso em milissegundos (2 segundos)
+    const atrasoDevagar = 7000
+    alert("olá rodrigo avalia")
+    console.log('Olá avalia no navegador')
+
+    setTimeout(function () {
+        btn.style.display = "none"
+        audio.play()
+    }, atrasoRapido);
+
+    setTimeout(function () {
+        elementoTexto.textContent = textoOriginal
+    }, atrasoDevagar);
+
+
+}
